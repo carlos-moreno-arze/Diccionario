4a5e32fd37dbf8a03be2385b6c70ae4d12264f76
keevj <jhonatan72700606@gmail.com>
2022-04-18 22:55:44 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Diccionario
-{
-    class program
-    {
-        static void main(string[] args)
-        {
-            Console.WriteLine("hola mundo");
-        }
+namespace Diccionario {
+  class program {
+    static void main(string[] args) {
+      Console.WriteLine("hola mundo");
     }
+  }
 }
 
