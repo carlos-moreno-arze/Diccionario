fd3e8bd345e7c820212bc8b3d02e43fd7ef4a247
carlos-moreno-arze <78823949+carlos-moreno-arze@users.noreply.github.com>
2022-04-18 22:53:38 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diccionario
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+        }
+    }
+}
+
